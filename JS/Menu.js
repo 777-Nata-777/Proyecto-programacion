258e6f52004ea871696b642250efdca9dfcc19e2
Jose Natanael Tejerina <jtejerina51@gmail.com>
2025-06-18 15:39:26 -0300

    Maxwell

--- JS/Menu.js
+++ JS/Menu.js
@@ -8,14 +8,14 @@ document.querySelectorAll('.button').forEach(button => button.addEventListener('
 }));
 /*Aviso*/
 document.addEventListener('DOMContentLoaded', () => {
-    const audio = new Audio('');
+    const audio = new Audio('/Audio/maxwell-nachorapista.mp3');
     audio.loop = true;
     const modal = document.getElementById('modal');
     const okBtn = document.getElementById('ok-btn');
     okBtn.addEventListener('click', () => {
         audio.play().catch((err) => {
-        console.warn("Error al intentar reproducir el audio:", err);
-    });
+            console.warn("Error al intentar reproducir el audio:", err);
+        });
         modal.style.display = 'none';
     });
 });
